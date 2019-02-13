6a2f510fc6b291b747224f92a28793a7c91cf1a5
Jacob Lundberg <jacob_lundberg@hotmail.com>
2019-02-13 16:15:07 +0100

    fix table

--- Ovning_JQuery_Ajax/wwwroot/js/site.js
+++ Ovning_JQuery_Ajax/wwwroot/js/site.js
@@ -2,3 +2,28 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+$(document).ready(function () {
+    var projektLista = $('#ProjektLista');
+    var pListan = $('<table></table>');
+    $.ajax({
+        url: "https://api.github.com/users/JacobLundberg/repos",
+        type: 'GET',
+        dataType: 'json'
+    })
+        .done(function (json) {
+            console.log(json.length);
+            pListan.append("<thead><tr><th>Name</th><th>Url</th></tr></thead>");
+            pListan.append("<tbody></tbody>");
+            for (var i = 0; i < json.length; i++) {
+                pListan.append("<tr><td>" + json[i].name + "</td><td>" + json[i].url + "</td></tr>");
+            }
+        })
+        .fail(function () {
+            alert("Sorry there was a problem");
+        });
+    projektLista.hide().html(pListan).fadeIn();
+    $("table").addClass("table").addClass("text-left").addClass("table-hover").addClass("table-bordered");
+});
+
+
