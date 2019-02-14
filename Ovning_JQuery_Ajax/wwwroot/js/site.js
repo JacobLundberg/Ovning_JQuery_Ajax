42cf956d83b5d772598490f4283c92fd19ed6f21
Jacob Lundberg <jacob_lundberg@hotmail.com>
2019-02-14 13:38:32 +0100

    refactor and fetch contributors

--- Ovning_JQuery_Ajax/wwwroot/js/site.js
+++ Ovning_JQuery_Ajax/wwwroot/js/site.js
@@ -4,8 +4,13 @@
 // Write your JavaScript code.
 
 $(document).ready(function () {
+    var repos = [];
     var projektLista = $('#ProjektLista');
     var pListan = $('<table></table>');
+    let contributorCount = 0;
+    //    let repo =  { name, created, contributors, numberOfContributors, url, contributors_url };
+
+    // fetch repos
     $.ajax({
         url: "https://api.github.com/users/JacobLundberg/repos",
         type: 'GET',
@@ -13,17 +18,47 @@ $(document).ready(function () {
     })
         .done(function (json) {
             console.log(json.length);
-            pListan.append("<thead><tr><th>Name</th><th>Created</th><th>Url</th></tr></thead>");
-            pListan.append("<tbody></tbody>");
             for (var i = 0; i < json.length; i++) {
-                pListan.append("<tr><td>" + json[i].name + "</td><td>" + json[i].created_at + "</td><td><a href=\"" + json[i].url + "\" >" + json[i].url+"</a></td></tr>");
+                repos.push({ name: json[i].name, created: json[i].created_at, url: json[i].url, contributors: '', numberOfContributors: 0, contributors_url: json[i].contributors_url });
             }
+            console.log(repos.length + ' in fetch repo.');
+            fetchContributers();
         })
         .fail(function () {
-            alert("Sorry there was a problem");
+            alert("Sorry there was a problem fetching repos");
         });
-    projektLista.hide().html(pListan).fadeIn();
+
+
+    // fetch contributers for each repo
+    function fetchContributers() {
+        for (var i = 0; i < repos.length; i++) {
+            $.ajax({
+                url: repos[i].contributors_url,
+                done: function (result) {
+                    repos[i].numberOfContributors = result.length;
+                    console.log(repos.length + ' in fetch contributers.');
+                    for (var j = 0; j < result.length; j++) {
+                        //                    repos[i].contributors += result[j].name;
+                        repos[i].contributors += j + 'name';
+                    }
+                    compileTable(pListan);
+                }
+            });
+        };
+    }
+
+    function compileTable(pListan) {
+        pListan.append("<thead><tr><th>Name</th><th>Created</th><th>#Contributors</th><th>Url</th></tr></thead>");
+        pListan.append("<tbody></tbody>");
+        console.log(repos.length + ' in compile table.');
+        for (var i = 0; i < repos.length; i++) {
+            pListan.append("<tr><td>" + repos[i].name + "</td><td>" + repos[i].created + "</td><td>" + repos[i].numberOfContributors + "</td><td><a href=\"" + repos[i].url + "\" >" + repos[i].url + "</a></td></tr>");
+        }
+        projektLista.hide().html(pListan).fadeIn();
+    }
     $("table").addClass("table").addClass("text-left").addClass("table-hover").addClass("table-bordered");
 });
+
+
 
 
